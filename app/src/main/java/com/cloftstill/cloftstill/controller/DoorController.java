8a2abe1c8b3f7dc081cf9a02d7d99727d1c92725
daniloBlera <danilo.cblera@gmail.com>
2016-05-11 15:19:55 -0300

    primeiro commit do projeto

---
+++ app/src/main/java/com/cloftstill/cloftstill/controller/DoorController.java
@@ -0,0 +1,16 @@
+package com.cloftstill.cloftstill.controller;
+
+/**
+ * Created by dcblera on 10/05/16.
+ */
+public class DoorController {
+    public void requestOpen(String request) {
+    }
+
+    private String getRequestMessage() {
+        return "._.";
+    }
+
+    public void checkServerConnection() {
+    }
+}
