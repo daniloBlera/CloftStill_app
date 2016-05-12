976c2c808708e9ad8f4ec72064bea149d695247a
daniloBlera <danilo.cblera@gmail.com>
2016-05-11 23:22:36 -0300

    servidor fala com app!

--- app/src/main/java/com/cloftstill/cloftstill/view/OpenDoorActivity.java
+++ app/src/main/java/com/cloftstill/cloftstill/view/OpenDoorActivity.java
@@ -3,28 +3,148 @@ package com.cloftstill.cloftstill.view;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.StrictMode;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.cloftstill.cloftstill.R;
-import com.cloftstill.cloftstill.controller.DoorController;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.protocol.BasicHttpContext;
+import org.apache.http.protocol.HttpContext;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 public class OpenDoorActivity extends AppCompatActivity {
     Context context = this;
 
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
+    /*@Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "OpenDoor Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.cloftstill.cloftstill.view/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "OpenDoor Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.cloftstill.cloftstill.view/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
+    }*/
+
+
+    public class LongRunningGetIO extends AsyncTask<String, Void, String> {
+        public String texto;
+
+        public String getTexto() {
+            return texto;
+        }
+
+        public void setTexto(String texto) {
+            this.texto = texto;
+        }
+
+
+        protected String getASCIIContentFromEntity(HttpEntity entity) throws IllegalStateException, IOException {
+            InputStream in = entity.getContent();
+            StringBuffer out = new StringBuffer();
+            int n = 1;
+            while (n > 0) {
+                byte[] b = new byte[4096];
+                n = in.read(b);
+                if (n > 0) out.append(new String(b, 0, n));
+            }
+            return out.toString();
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            HttpClient httpClient = new DefaultHttpClient();
+            HttpContext localContext = new BasicHttpContext();
+            String urlString = params[0];
+            HttpGet httpGet = new HttpGet(urlString);
+            //String texto = null;
+
+            try {
+                HttpResponse response = httpClient.execute(httpGet, localContext);
+                HttpEntity entity = response.getEntity();
+                texto = getASCIIContentFromEntity(entity);
+
+            } catch (Exception e) {
+                return e.getLocalizedMessage();
+            }
+
+            return texto;
+        }
+    }
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_open_door);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-        isNetworkAvaliable();
+        final String t;
+
+        // isNetworkAvaliable();
+
+        final LongRunningGetIO L = new LongRunningGetIO();
+        L.execute("http://192.168.0.5:5000/open/");
+
+        try {
+            L.get(); // ESPERA EXECUTAR A ASYNC TASK
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        t = L.getTexto();
+
 
         /**
          * Liberar permissões
@@ -32,17 +152,17 @@ public class OpenDoorActivity extends AppCompatActivity {
 //        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
 //        StrictMode.setThreadPolicy(policy);
 
-        setContentView(R.layout.activity_open_door);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+
 
         Button openDoorBtn = (Button) findViewById(R.id.openDoorButton);
         assert openDoorBtn != null;
         openDoorBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick (View view) {
-                DoorController controller = new DoorController();
-                controller.requestOpen();
+            public void onClick(View view) {
+                //t = L.getTexto();
+                //DoorController controller = new DoorController();
+                //controller.requestOpen();
+                Toast.makeText(context,t, Toast.LENGTH_LONG).show();
             }
         });
 
@@ -54,6 +174,9 @@ public class OpenDoorActivity extends AppCompatActivity {
 //                        .setAction("Action", null).show();
 //            }
 //        });
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     @Override
